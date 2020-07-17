ab2bd6eeebccf97f3e4cdaacb583639682b44ee3
acd02 <alex.acd02@gmail.com>
2020-07-17 18:31:09 +0200

    feat: add custom tailwind variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,23 @@ module.exports = {
     extend: {},
   },
   variants: {
-    textColor: ['responsive', 'hover', 'focus', 'important'],
+    textColor: ['responsive', 'hover', 'focus', 'important', 'important-hover'],
   },
   plugins: [
     plugin(function ({ addVariant }) {
       addVariant('important', ({ container }) => {
         container.walkRules(rule => {
           rule.selector = `.\\!${rule.selector.slice(1)}`
+          rule.walkDecls(decl => {
+            decl.important = true
+          })
+        })
+      })
+    }),
+    plugin(function ({ addVariant }) {
+      addVariant('important-hover', ({ container }) => {
+        container.walkRules(rule => {
+          rule.selector = `.\\!hover-${rule.selector.slice(1)}:hover`
           rule.walkDecls(decl => {
             decl.important = true
           })
