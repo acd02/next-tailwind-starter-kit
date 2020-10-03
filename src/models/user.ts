819dca7fa9612c24f7912ce48fdaf87459c48bc8
acd02 <alex.acd02@gmail.com>
2020-10-03 15:59:16 +0200

    chore: update packages

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,12 +1,6 @@
-export type User = {
-  id: number
-  name: string
-  username: string
-  email: string
-  address: Address
-  phone: string
-  website: string
-  company: Company
+type Geo = {
+  lat: string
+  lng: string
 }
 
 type Address = {
@@ -17,13 +11,19 @@ type Address = {
   geo: Geo
 }
 
-type Geo = {
-  lat: string
-  lng: string
-}
-
 type Company = {
   name: string
   catchPhrase: string
   bs: string
+}
+
+export type User = {
+  id: number
+  name: string
+  username: string
+  email: string
+  address: Address
+  phone: string
+  website: string
+  company: Company
 }
