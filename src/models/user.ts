de09d53a6457a0bd50264093a39682e324dd6e5a
acd02 <alex.acd02@gmail.com>
2020-11-13 18:46:35 +0100

    feat: set up "prismic"

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,29 +1,6 @@
-type Geo = {
-  lat: string
-  lng: string
-}
-
-type Address = {
-  street: string
-  suite: string
-  city: string
-  zipcode: string
-  geo: Geo
-}
-
-type Company = {
-  name: string
-  catchPhrase: string
-  bs: string
-}
-
 export type User = {
-  id: number
+  id: string
   name: string
-  username: string
   email: string
-  address: Address
-  phone: string
-  website: string
-  company: Company
+  address: { street: string; city: string; zipcode: number }[]
 }
