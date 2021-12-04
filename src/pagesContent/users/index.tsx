15728183d9bde348d1a449aa77b8f1fd044feb8c
acd02 <alex.acd02@gmail.com>
2021-12-04 18:06:39 +0100

    refactor: clean up code

--- src/pagesContent/users/index.tsx
+++ src/pagesContent/users/index.tsx
@@ -1,8 +1,7 @@
 import { Button } from 'components/atoms/Button'
+import { Link } from 'components/atoms/Link'
 import { useState } from 'react'
 import { User } from 'types/user'
-
-import { UserLink } from './components/UserLink'
 
 type Props = {
   users: User[]
@@ -11,13 +10,19 @@ type Props = {
 export function RenderUsers({ users }: Props) {
   const [showUsers, setShowUsers] = useState(true)
 
+  const links = users.map(({ id, name }) => (
+    <li className="block mb-2 w-[fit-content]">
+      <Link route="users" param={String(id)} label={name} />
+    </li>
+  ))
+
   return (
     <>
       <Button onClick={() => setShowUsers(s => !s)}>
         {showUsers ? 'hide' : 'show'} users
       </Button>
       <h2 className="mb-4 text-4xl">Users:</h2>
-      {showUsers && users.map(u => <UserLink key={u.id} {...u} />)}
+      <ul>{showUsers && links}</ul>
     </>
   )
 }
